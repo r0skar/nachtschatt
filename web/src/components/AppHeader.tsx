4819f27da24dc7fe5c03e0dc71fb65fc01cd1fd2
Oskar Rainer <rainer.oskar@gmail.com>
2020-02-22 15:22:04 +0100

    add sub title

--- web/src/components/AppHeader.tsx
+++ web/src/components/AppHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Wrap } from './UI'
 
 interface Props {
@@ -36,6 +36,11 @@ const SubTitle = styled.h2`
   font-size: 1.5rem;
   text-transform: uppercase;
   padding-left: 1ch;
+
+  /* Only hide it visually in order to keep the grid layout as is. */
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.sm}px) {
+    visibility: hidden;
+  }
 `
 
 const NavButton = styled.button`
@@ -45,22 +50,28 @@ const NavButton = styled.button`
   width: 2rem;
 `
 
-export const AppHeader: React.FC<Props> = ({ toggleNav, hasSolidBg }) => (
-  <Container hasSolidBg={hasSolidBg}>
-    <Wrap fillHeight>
-      <Grid>
-        <MainTitle>
-          <Link to="/">Site Title</Link>
-        </MainTitle>
-        <SubTitle>SubTitle</SubTitle>
-        <NavButton onClick={toggleNav}>
-          <svg viewBox="0 0 384.97 384.97">
-            <path d="M12.03,120.303h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03 c-6.641,0-12.03,5.39-12.03,12.03C0,114.913,5.39,120.303,12.03,120.303z" />
-            <path d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03 S379.58,180.455,372.939,180.455z" />
-            <path d="M372.939,264.667H132.333c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h240.606 c6.641,0,12.03-5.39,12.03-12.03C384.97,270.056,379.58,264.667,372.939,264.667z" />
-          </svg>
-        </NavButton>
-      </Grid>
-    </Wrap>
-  </Container>
-)
+export const AppHeader: React.FC<Props> = ({ toggleNav, hasSolidBg }) => {
+  const { pathname } = useLocation()
+  const [, p1, p2] = pathname.split('/')
+  const pageTitle = p2 || p1 || ''
+
+  return (
+    <Container hasSolidBg={hasSolidBg}>
+      <Wrap fillHeight>
+        <Grid>
+          <MainTitle>
+            <Link to="/">Site Title</Link>
+          </MainTitle>
+          <SubTitle>{pageTitle}</SubTitle>
+          <NavButton onClick={toggleNav}>
+            <svg viewBox="0 0 384.97 384.97">
+              <path d="M12.03,120.303h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03 c-6.641,0-12.03,5.39-12.03,12.03C0,114.913,5.39,120.303,12.03,120.303z" />
+              <path d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03 S379.58,180.455,372.939,180.455z" />
+              <path d="M372.939,264.667H132.333c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h240.606 c6.641,0,12.03-5.39,12.03-12.03C384.97,270.056,379.58,264.667,372.939,264.667z" />
+            </svg>
+          </NavButton>
+        </Grid>
+      </Wrap>
+    </Container>
+  )
+}
